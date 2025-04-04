380265daa8f23504b6653e6ef48462182caa59b6
Wannes Gennar <wannes.gennar@gmail.com>
2025-04-04 19:25:35 +0200

    update journal support for music and scene links

--- src/scripts/journals/text-page.ts
+++ src/scripts/journals/text-page.ts
@@ -1,7 +1,8 @@
 export default class AdventureJournalTextPageSheet extends JournalTextPageSheet {
     // register your journal's custom listeners.
     activateListeners(html: JQuery): void {
-        $(html).children('.narrate').on('click', this.#narrate);
+        $(html).find('.narrate').on('click', this.#narrate);
+        $(html).find('[data-id][data-type="Scene"]').on('click', this.#openScene);
 
         super.activateListeners(html);
     }
@@ -13,7 +14,7 @@ export default class AdventureJournalTextPageSheet extends JournalTextPageSheet 
     protected async _renderInner(data: ReturnType<this["getData"]>): Promise<JQuery<HTMLElement>> {
         const result = await super._renderInner(data);
         this.#updateImages(result);
-        result.find('[data-id][data-type="Scene"]').on('click', this.#openScene);
+        this.#updateSounds(result);
 
         return result;
     }
@@ -29,6 +30,28 @@ export default class AdventureJournalTextPageSheet extends JournalTextPageSheet 
             // Append if not already present
             if (!existingStyle.includes("shape-outside")) {
                 $img.attr("style", `${existingStyle} ${shapeOutsideRule}`.trim());
+            }
+        });
+    }
+
+    // Automatically update ambient sound links to have the music icon similar to rolls etc and make them clickable to toggle the sound linked.
+    #updateSounds(content: JQuery<HTMLElement>) {
+        content.find('[data-id][data-type="AmbientSound"]').each((_, link) => {
+            const id: string = $(link).data('id');
+            const sound = game.canvas.sounds.get(id)
+
+            if (sound) {
+                const icon = sound.document.hidden ? 'play' : 'stop';
+                $(link).html(`<i class="fas fa-${icon}"></i> ${$(link).text()}`);
+
+                $(link).on('click', async (event) => {
+                    event.preventDefault();
+                    event.stopPropagation();
+            
+                    await (sound as any).document.update({ hidden: !sound.document.hidden });
+                    const icon = sound.document.hidden ? 'play' : 'stop';
+                    $(link).html(`<i class="fas fa-${icon}"></i> ${$(link).text()}`);
+                });
             }
         });
     }
