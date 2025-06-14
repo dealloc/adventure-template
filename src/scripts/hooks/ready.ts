344f109a993b81634835554fdb1bf7472de6d6f0
Wannes Gennar <wannes.gennar@gmail.com>
2025-06-14 15:55:08 +0200

    reduce to warning when adventure is not configured

--- src/scripts/hooks/ready.ts
+++ src/scripts/hooks/ready.ts
@@ -2,6 +2,11 @@ import CONSTANTS from "../constants";
 import { getSetting } from "../settings";
 
 export async function onReady() {
+    if (CONSTANTS.adventure === null) {
+        console.warn(`Adventure Template: No adventure compendium specified in constants.ts. Please set CONSTANTS.adventure to the ID of your adventure compendium.`);
+        return;
+    }
+
     const pack = game.packs.get(`${CONSTANTS.id}.${CONSTANTS.adventure}`);
 
     if (getSetting<boolean>('showImport') !== false) {
